339fd33288333392c9bfe6d6974c4033ff6f28bf
alvaro.calper <alvaro.calper@ED49000391P301.educa.jcyl.es>
2024-12-19 11:57:48 +0100

    Inicio proyecto

---
+++ src/main/java/com/mycompany/prueba1_eedd/Prueba1_eedd.java
@@ -0,0 +1,16 @@
+/*
+ * Carvajal balón de oro
+ */
+
+package com.mycompany.prueba1_eedd;
+
+/**
+ *
+ * @author alvaro.calper
+ */
+public class Prueba1_eedd {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
